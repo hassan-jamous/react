4994f65f9427b46b4d82919adf6727fa85432eca
Hassan Jamous <hassan.jamous@nab.com.au>
2018-12-24 15:28:04 +1100

    remove comment

--- src/logic/functions/login.function.ts
+++ src/logic/functions/login.function.ts
@@ -1,9 +1,5 @@
 import { LoginRequest } from '../domains/loginRequest.domain';
 
-// add response validation
-// make it class
-// use axios
-
 export function tryLogin(loginRequest: LoginRequest) {
   alert(loginRequest.pWord + "   " + loginRequest.pWord);
 }
